55e0357ab9f6bc274b68e05443e321cc7910aacf
Aaron Humphrey <aarhumph@indiana.edu>
2017-10-12 23:39:07 -0400

    refactor to break up setting

--- src/redux/actions/creators/accountActions.js
+++ src/redux/actions/creators/accountActions.js
@@ -7,35 +7,6 @@ import { SIGN_IN, SIGN_UP, SIGN_OUT } from '../types/accountActionTypes'
  which can be used inside the function to dispatch events.
  In this case we dispatch the signIn actions and then then it is finished we dispatch a push to the router to go the /dashboard */
 
-// const migrateOrUpdate = (firstName, lastName, photoFile, email) => {
-//   const userMigrationRef = firebase.database().ref('/userMigration')
-//   userMigrationRef.orderByChild('email').equalTo(email).once('value').then((snapshot) => {
-//     const dataToMigrate = snapshot.val()
-//     if (dataToMigrate !== null) {
-//       const uid = firebase.auth().currentUser.uid
-//       const key = Object.keys(dataToMigrate)[0]
-//       const userToMigrate = dataToMigrate[key]
-//       const roles = userToMigrate.roles
-//       const firstName = userToMigrate.firstName
-//       const lastName = userToMigrate.lastName
-//       const profilesRef = firebase.database().ref(`/userProfiles/${uid}/roles`)
-//       const accountRef = firebase.database().ref(`/userAccount/${uid}`)
-//       profilesRef.set(roles)
-//       accountRef.set({
-//         firstName,
-//         lastName,
-//       })
-//     }
-//   })
-//   const uid = firebase.auth().currentUser.uid
-//   const accountRef = firebase.database().ref(`/userAccount/${uid}`)
-//   accountRef.set({
-//     firstName,
-//     lastName,
-//     photoFile
-//   })
-// }
-
 const migrateIfNeeded = (email) => {
   const userMigrationRef = firebase.database().ref('/userMigration')
   userMigrationRef.orderByChild('email').equalTo(email).once('value').then((snapshot) => {
@@ -58,17 +29,25 @@ const migrateIfNeeded = (email) => {
   })
 }
 
-const updateAccount = (firstName, lastName, photoFile) => {
+const updateAccount = (firstName, lastName, email, photoFile) => {
 	const uid = firebase.auth().currentUser.uid
-	const fbFilePath = `/images/users/account/${uid}/account_image`
-	firebase.UserInfo.uploadFile(fbFilePath, photoFile).then((response) => {
-		const accountRef = firebase.database().ref(`/userAccount/${uid}`)
-		accountRef.set({
-			firstName: firstName,
-			lastName: lastName,
-			photoUrl: response.downloadURL,
+	const accountRef = firebase.database().ref(`/userAccount/${uid}`)
+
+	if (firstName) {
+		accountRef.child('firstName').set(firstName)
+	}
+	if (lastName) {
+		accountRef.child('lastName').set(lastName)
+	}
+	if (email) {
+		accountRef.child('email').set(email)
+	}
+	if (photoFile) {
+		const fbFilePath = `/images/users/account/${uid}/account_image`
+		firebase.uploadFile(fbFilePath, photoFile).then((response) => {
+			accountRef.child('photoURL').set(response.downloadURL)
 		})
-	})
+	}
 }
 
 export const signIn = (email, password) => dispatch => dispatch({
@@ -82,7 +61,7 @@ export const signUp = (firstName, lastName, photoFile, email, password) => dispa
 }).then(() => dispatch(push('account'))).then(
         () => {
         	migrateIfNeeded(email)
-	        updateAccount(firstName, lastName, photoFile)
+	        updateAccount(firstName, lastName, email, photoFile)
         })
 
 export const signOut = () => dispatch => dispatch({
